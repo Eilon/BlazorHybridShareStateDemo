40ea5642bb3351a10f86dd83c0b5bf9bab0ff011
Eilon Lipton <Eilon@users.noreply.github.com>
2022-11-07 13:06:12 -0800

    Add SharedState type

---
+++ BlazorHybridShareStateDemo/MySharedState.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorHybridShareStateDemo
+{
+    public class MySharedState
+    {
+        public int CurrentCount { get; private set; }
+
+        public event EventHandler CountChanged;
+
+        public void IncrementCount()
+        {
+            CurrentCount++;
+            CountChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
