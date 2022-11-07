24be6aec7ea53b0843edf64b5754229b6eda5c28
Eilon Lipton <Eilon@users.noreply.github.com>
2022-11-07 10:45:51 -0800

    Add native UI for button counter

--- BlazorHybridShareStateDemo/MainPage.xaml.cs
+++ BlazorHybridShareStateDemo/MainPage.xaml.cs
@@ -5,5 +5,22 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+		BindingContext = this;
+
+		UpdateLabelText();
 	}
+
+    private void UpdateLabelText()
+    {
+        LabelText = $"Count is: {currentCount++}";
+        OnPropertyChanged(nameof(LabelText));
+    }
+
+    private int currentCount;
+	public string LabelText { get; set; }
+
+	private void Button_Clicked(object sender, EventArgs e)
+    {
+        UpdateLabelText();
+    }
 }
